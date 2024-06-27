5b080bd0bdf3121118fb0249f81b3e8f1ad216f1
nitindhakad77 <95351272+nitindhakad77@users.noreply.github.com>
2024-06-28 00:24:33 +0530

    Update simpleTable.js

--- src/components/simpleTable.js
+++ src/components/simpleTable.js
@@ -1,20 +1,45 @@
 import { React } from "react";
+import {Table} from "antd"
+import { Button } from "antd";
+import 'antd/dist/antd.css';
 
-const SimpleTable = ({ dataSource }) => {
+const SimpleTable = ({ dataSource,editUser, deleteUser}) => {
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      render: (text, record) => (
+        <span className="edit">
+          <Button type="primary" onClick={() => editUser(record)}>
+            Edit
+          </Button>
+          <Button type="primary" danger onClick={() => deleteUser(record.id)} style={{ marginLeft: 8 }}>
+            Delete
+          </Button>
+        </span>
+      ),
+    },  
+    
+  ];
   return (
-    <div>
+    <div className="table-container">
       {dataSource.length ? (
-        <>
-          {dataSource.map((item, index) => {
-            return (
-              <div key={index}>
-                <div>Id: {item.id}</div>
-                <div>Name: {item.name}</div>
-                <div>Email: {item.email}</div>
-              </div>
-            );
-          })}
-        </>
+        <Table dataSource={dataSource} columns={columns} rowKey="id" />
       ) : (
         "No user data"
       )}
